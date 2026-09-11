d4baef3e06381310d8bfb47285a56d179d2edd65
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-12 00:22:39 +0700

    feat(semantic): export V2 composer foundation

--- src/index.ts
+++ src/index.ts
@@ -64,3 +64,4 @@ export * from "./components/perspective-intelligence"
 
 export * from "./components/web-stats"
 export * from "./components/observatory-footer"
+export * from "./contracts/semantic-composer"
